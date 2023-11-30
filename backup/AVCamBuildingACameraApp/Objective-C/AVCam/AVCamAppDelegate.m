c4aa77781ace1974a00d3553b4e4802e4b44bc4a
LinkJin <jinj@8btc.com>
2023-11-30 16:01:18 +0800

    文件夹替换

---
+++ backup/AVCamBuildingACameraApp/Objective-C/AVCam/AVCamAppDelegate.m
@@ -0,0 +1,12 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+The app's delegate object.
+*/
+
+#import "AVCamAppDelegate.h"
+
+@implementation AVCamAppDelegate
+
+@end
